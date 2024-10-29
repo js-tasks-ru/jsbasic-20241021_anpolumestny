cd29ef0db45276269297e9d1db67acbb4ef3c3b8
apolumestny <anpolumestny@gmail.com>
2024-10-29 21:26:41 +0300

    sumSalary func

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let salary = 0;
+
+  for ( let prop in salaries) {
+    if (Number.isFinite(salaries[prop])) {
+      salary += salaries[prop];
+    }
+  }
+  return salary;
 }
