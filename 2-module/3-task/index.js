44f9f6e6869cb15b90ec0ceb98a08c6c3da7bbff
apolumestny <anpolumestny@gmail.com>
2024-11-04 15:47:43 +0300

    3 module

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -13,8 +13,4 @@ let calculator = {
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
-// calculator.read(1, 0)
-// console.log(calculator.sum())
-// console.log(calculator.mul())
-// console.log(calculator.a)
-// console.log(calculator.b)
+
