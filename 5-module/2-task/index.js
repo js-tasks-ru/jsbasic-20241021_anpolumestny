edd0a1cfa24a6f474009742a8cb46d94ac01c148
apolumestny <anpolumestny@gmail.com>
2024-11-24 20:14:24 +0300

    5 module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  let botton = document.querySelector('.toggle-text-button');
+  botton.addEventListener('click', function(event){
+    let textElement = document.getElementById('text');
+    textElement.hidden = !textElement.hidden;
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  let mainCarousel = document.querySelector('.carousel__inner');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let currentOffset = 0;
+  const slideWidth = mainCarousel.offsetWidth;
+  const totalSlides = mainCarousel.children.length;
+
+  function updateArrow() {
+    arrowLeft.style.display = currentOffset === 0 ? 'none' : '';
+    arrowRight.style.display = currentOffset === -(totalSlides - 1) * slideWidth ? 'none' : '';
+  }
+
+  function moveCarousel(direction, mainCarousel) {
+    console.log(direction);
+    console.dir(mainCarousel);
+
+    if (direction === 'right') {
+      currentOffset -= slideWidth;
+    } else if (direction === 'left') {
+      currentOffset += slideWidth;
+    }
+    const maxOffset = -((totalSlides - 1) * slideWidth);
+    currentOffset = Math.max(Math.min(currentOffset, 0), maxOffset);
+    mainCarousel.style.transform = `translateX(${currentOffset}px)`;
+    updateArrow();
+  }
+
+  arrowRight.addEventListener('click', () => moveCarousel('right', mainCarousel));
+  arrowLeft.addEventListener('click', () => moveCarousel('left', mainCarousel));
+  updateArrow();
 }
+
+
+
+
